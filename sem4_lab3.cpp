a61686415d5481a74463d0ad7a0a41b30ec6a863
iseda <iseda@DESKTOP-IKCIUDU>
2024-04-13 17:33:33 +0300

    Добавьте файлы проекта.

---
+++ sem4_lab3.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <string>
+#include "Shape.h"
+#include "Shape2D.h"
+#include "Shape3D.h"
+#include "Square.h"
+#include "Triangle.h"
+#include "Circle.h"
+#include "TriangularPyramid.h"
+#include "Cylinder.h"
+#include "Sphere.h"
+
+int main() {
+	Square sq(3.0);
+	sq.CalculateArea();
+	sq.ShowInfo();
+
+	Triangle tr(2, 5);
+	tr.CalculateArea();
+	tr.ShowInfo();
+
+	Circle cr(5);
+	cr.CalculateArea();
+	cr.ShowInfo();
+
+	TriangularPyramid pyramid(6, 5, 4);
+	pyramid.CalculateVolume();
+	pyramid.ShowInfo();
+
+	Cylinder cyl(7, 2);
+	cyl.CalculateVolume();
+	cyl.ShowInfo();
+
+
+	Sphere sp(4);
+	sp.CalculateVolume();
+	sp.ShowInfo();
+
+
+	return 0;
+}
